c69565286608d6af8a8ceced759a21852d6f6423
jmario mejia <jmariomejiap@gmail.com>
2017-12-26 12:18:48 -0800

    adding basic functionality, git-info module, fs.readFile

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as getRepoInfo from 'git-repo-info';
+
+
+const pathToFile: string = './coverageData/coverage-summary.json'
+
+const fileReader = (file: string) => {
+  return new Promise((resolve, reject) => {
+    return fs.readFile(path.resolve(file), (err, data) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(data);
+    });
+  })
+};
+
+
+const gitReader = () => {
+  const info = getRepoInfo();
+
+  const gitInfo = {
+    gitInfo: {
+      branch: info.branch,
+      sha: info.sha,
+      abbreviatedSha: info.abbreviatedSha,
+      author: info.author,
+    }
+  };
+
+  return gitInfo;   
+};
+
+
+const fetchJsonCoverage = async () => {
+  let result;
+  try {
+    result = await fileReader(pathToFile);
+  } catch (error) {
+    console.error('catching error = ', error);
+  }
+
+  return result;
+};
+
+
+
+
 
 const msg: string = 'Code Quality CLI';
 console.log(msg);
+
+console.log('******');
+console.log(fetchJsonCoverage());
