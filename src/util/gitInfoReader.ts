f2a2e15be7ec02982c68acc7af424e26e249f033
jmario mejia <jmariomejiap@gmail.com>
2018-01-23 14:28:01 -0800

    adding regex to capture branch name HARD PUSH5

--- src/util/gitInfoReader.ts
+++ src/util/gitInfoReader.ts
@@ -4,15 +4,15 @@ const gitReader = async (): Promise<any> => {
       .log((err, log) => {
         const { hash, message, author_name } = log.latest;
         console.log('log = ', log.latest);
-        const regexFindBranch = /(?<=origin\/).[a-z, 0-9]+/g;
+        // const regexFindBranch = /(?<=origin\/).[a-z, 0-9]+/g;
         
-        let branch;
+        let branch : 'hardCoded';
         try {
-          branch = message.match(regexFindBranch)[0];
+          // branch = message.match(regexFindBranch)[0];
         } catch (error) {
           console.error('internal_error');
         }
-        
+
         console.log('sliced = ', branch);
 
         const gitInfo = {
