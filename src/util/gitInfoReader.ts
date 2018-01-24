55a4bd701a1743ca6b058f0aaa75578da73f5071
Oscar Mejia <oscmejia@gmail.com>
2018-01-23 20:47:03 -0500

    refactor

--- src/util/gitInfoReader.ts
+++ src/util/gitInfoReader.ts
@@ -1,9 +1,10 @@
 import { GitInfo, takeStringReturnStringFunc } from '../util/types/indexTypes';
-
+const simpleGit = require('simple-git')();
 const gitReader: takeStringReturnStringFunc = (stringValue) => {
   return new Promise((resolve) => {
-    require('simple-git')()
+    simpleGit
       .log((err, log) => {
+        // simpleGit.branch...
         const { hash, message, author_name } = log.latest;
 
         const branch = message.slice(message.indexOf('origin/') + 7, message.length - 1);
